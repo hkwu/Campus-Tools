975d83d3698764013455747a6048c03f29ff20a6
hkwu <kelvinwu1996@gmail.com>
2014-12-19 14:58:18 -0500

    removing version

--- interface.py
+++ interface.py
@@ -11,34 +11,33 @@ def scrn_clr():
 
 # Base menu class
 class Menu(object):
-    def __init__(self, menu_name, version, *args):
+    def __init__(self, menu_name, *args):
         self.menu_name = menu_name
-        self.version = version
         self.args = args
 
     def __str__(self):
         avail_options = []
         for option in self.args:
             avail_options.append(option)
-        print "{} v{}\nCoded by {}" \
-            .format(self.menu_name, self.version, __author__)
+        print "{} \nCoded by {}" \
+            .format(self.menu_name,  __author__)
         print "=" * 20, "\n"
         for option in avail_options:
             print "{}: {}".format(avail_options.index(option) + 1, option)
         print ""
 
     def title(self):
-        print "{} v{}\nCoded by {}" \
-            .format(self.menu_name, self.version, __author__)
+        print "{} \nCoded by {}" \
+            .format(self.menu_name, __author__)
         print "=" * 20, "\n"
 
 
 # The initial menu screen
 class TopMenu(Menu):
     def __init__(self):
-        super(TopMenu, self).__init__("CAMPUS TOOLS", 1.0,
-                                      "CAMPUS TUTORS", "CAMPUS FOOD",
-                                      "CAMPUS WEATHER", "EXIT")
+        super(TopMenu, self).__init__("CAMPUS TOOLS", "CAMPUS TUTORS",
+                                      "CAMPUS FOOD", "CAMPUS WEATHER",
+                                      "EXIT")
 
     def choice(self):
         usr_input = raw_input("> ").upper()
@@ -67,7 +66,7 @@ class TopMenu(Menu):
 # The Campus Tutors menu
 class CampusTutors(Menu):
     def __init__(self):
-        super(CampusTutors, self).__init__("CAMPUS TUTORS", 1.02,
+        super(CampusTutors, self).__init__("CAMPUS TUTORS",
                                            "SEARCH FOR TUTORS", "RETURN")
 
     def choice(self):
@@ -90,13 +89,17 @@ class CampusTutors(Menu):
 # The Campus Food menu
 class CampusFood(Menu):
     def __init__(self):
-        super(CampusFood, self).__init__("CAMPUS FOOD", 1.03,
+        super(CampusFood, self).__init__("CAMPUS FOOD", "WHAT'S OPEN?",
                                          "SEARCH FOR MENUS", "RETURN")
 
     def choice(self):
         usr_input = raw_input("> ").upper()
-        while usr_input not in ["2", "RETURN"]:
-            if usr_input in ["1", "SEARCH FOR MENUS"]:
+        while usr_input not in ["3", "RETURN"]:
+            if usr_input in ["1", "WHAT'S OPEN?"]:
+                scrn_clr()
+                import menu
+                menu.open_user_enters()
+            elif usr_input in ["2", "SEARCH FOR MENUS"]:
                 scrn_clr()
                 import menu
                 menu.menu_user_enters()
@@ -113,9 +116,8 @@ class CampusFood(Menu):
 # The Campus Weather menu
 class CampusWeather(Menu):
     def __init__(self):
-        super(CampusWeather, self).__init__("CAMPUS WEATHER", 1.0,
-                                            "BASIC WEATHER", "TRIVIAL WEATHER",
-                                            "RETURN")
+        super(CampusWeather, self).__init__("CAMPUS WEATHER", "BASIC WEATHER",
+                                            "TRIVIAL WEATHER", "RETURN")
 
     def choice(self):
         usr_input = raw_input("> ").upper()
