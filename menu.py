d7b70fdfdc63fb089861885eee8a7d06c2421eae
hkwu <kelvinwu1996@gmail.com>
2014-12-20 14:57:10 -0500

    adding nutrition info

--- menu.py
+++ menu.py
@@ -9,8 +9,8 @@ locations = uw.locations()
 
 
 # ====================================================
-# curr_menu() outputs the daily menu
 def curr_menu(location, meal, day):
+    """curr_menu() outputs the daily menu"""
     for outlet in menu['outlets']:
         if outlet['outlet_name'].lower() == location:
             outlet_name = outlet['outlet_name']
@@ -26,24 +26,25 @@ def curr_menu(location, meal, day):
         print "\nSorry, we couldn't find that outlet's menu!\n"
 
 
-# meal_getter() outputs the dishes for the meal type
 def meal_getter(meal, daily_menu, location, day):
+    """outputs the dishes for the meal type"""
     if meal in ["lunch", "dinner"]:
         avail_dishes = daily_menu[meal]
         print "\nHere is the {} {} menu for {}:" \
             .format(day, meal, location)
         print "-" * 20
         for dish in avail_dishes:
-            print "Dish: {}\nType: {}".format(dish['product_name'],
-                                              dish['diet_type'])
+            print "Dish: {}\nType: {}\nID: {}".format(dish['product_name'],
+                                                      dish['diet_type'],
+                                                      dish['product_id'])
             print "-" * 20
         print ""
     else:
         print "\nSorry, we don't serve that here!\n"
-		
-		
-# menu_user_enters() gets the input for curr_menu()
+
+
 def menu_user_enters():
+    """gets the input for curr_menu"""
     import interface
     interface.CampusFood().title()
     print "Enter your specifications.\n"
@@ -60,9 +61,9 @@ def menu_user_enters():
 
 
 # ====================================================
-# open_outlets() determines which outlets are open at the
-# current time
 def open_outlets():
+    """determines which outlets are open at the
+    current time"""
     open_now = []
     for outlet in locations:
         if outlet['is_open_now']:
@@ -70,12 +71,14 @@ def open_outlets():
     outlet_printer(open_now)
 
 
-# outlet_printer() takes a list of outlets and prints the
-# list, if non-empty
 def outlet_printer(lst_of_outlets):
+    """takes a list of outlets and prints the
+    list, if non-empty"""
     if lst_of_outlets:
         print "These places are open right now:\n", "-" * 20
         for outlet in lst_of_outlets:
+            if "\u2019" in outlet:
+                outlet = outlet.replace("\u2019", "'")
             print u"{}: {}".format(lst_of_outlets.index(outlet) + 1,
                                    outlet)
         print "-" * 20, "\n"
@@ -83,11 +86,68 @@ def outlet_printer(lst_of_outlets):
         print "\nSorry, it seems like no one's open right now!\n"
 
 
-# open_user_enters() is the wrapper for open_outlets()
 def open_user_enters():
+    """the wrapper for open_outlets"""
     import interface
     interface.CampusFood().title()
     open_outlets()
+    print "Press any key to return.\n"
+    raw_input("> ")
+    interface.scrn_clr()
+    interface.CampusFood().__str__()
+    interface.CampusFood().choice()
+# ====================================================
+
+
+# ====================================================
+def nutrition_info(prod_id):
+    """gets the nutrition info for the given product ID"""
+    product = uw.products(prod_id)
+    if product:
+        print "\nNutrition Information for {}\n{}\n" \
+            .format(product['product_name'],
+                    product['diet_type']), "-" * 20
+        print """Serving Size: {}
+Calories: {}
+Total Fat (g / %): {} / {}
+  Saturated Fat (g / %): {} / {}
+  Trans Fat (g / %): {} / {}
+Cholesterol (mg): {}
+Sodium (mg / %): {} / {}
+Carbohydrates (g / %): {} / {}
+  Fibre (g / %): {} / {}
+  Sugar (g): {}
+Protein (g): {}""" \
+            .format(product['serving_size'], product['calories'],
+                    product['total_fat_g'], product['total_fat_percent'],
+                    product['fat_saturated_g'], product['fat_saturated_percent'],
+                    product['fat_trans_g'], product['fat_trans_percent'],
+                    product['cholesterol_mg'], product['sodium_mg'],
+                    product['sodium_percent'], product['carbo_g'],
+                    product['carbo_percent'], product['carbo_fibre_g'],
+                    product['carbo_fibre_percent'], product['carbo_sugars_g'],
+                    product['protein_g'])
+        print "-" * 20
+        print """Vitamin A (%): {}
+Vitamin C (%): {}
+Calcium (%): {}
+Iron (%): {}
+Micronutrients: {}""" \
+            .format(product['vitamin_a_percent'], product['vitamin_c_percent'],
+                    product['calcium_percent'], product['iron_percent'],
+                    product['micro_nutrients'])
+        print "-" * 20, "\nIngredients: {}\n".format(product['ingredients']), \
+            "-" * 20
+        print "Consumption Tips: {}\n".format(product['tips']), "-" * 20, "\n"
+    else:
+        print "\nSorry, we couldn't find that dish!\n"
+
+
+def nutrition_user_enters():
+    """gets the product ID for nutrition_info"""
+    import interface
+    interface.CampusFood().title()
+    nutrition_info(int(raw_input("Enter your product ID: ")))
     print "Press any key to return.\n"
     raw_input("> ")
     interface.scrn_clr()
