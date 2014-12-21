7271afabc0290d4565a2e6a24ebb4e20e9645263
hkwu <kelvinwu1996@gmail.com>
2014-12-20 21:41:50 -0500

    decoding stuff

--- menu.py
+++ menu.py
@@ -77,8 +77,8 @@ def outlet_printer(lst_of_outlets):
     if lst_of_outlets:
         print "These places are open right now:\n", "-" * 20
         for outlet in lst_of_outlets:
-            if "\u2019" in outlet:
-                outlet = outlet.replace("\u2019", "'")
+            if u"\u2019" in outlet:
+                outlet = outlet.replace(u"\u2019", "'")
             print u"{}: {}".format(lst_of_outlets.index(outlet) + 1,
                                    outlet)
         print "-" * 20, "\n"
